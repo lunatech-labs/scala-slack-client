0dd984372971efcb9e4762a2c3f49bd5d9fa6bda
Herizo Kerharo <kherizo@gmail.com>
2018-07-04 11:15:22 +0200

    Refactoring code

--- src/main/scala/com/lunatech/slack/client/services/SlackCaller.scala
+++ src/main/scala/com/lunatech/slack/client/services/SlackCaller.scala
@@ -1,31 +1,40 @@
 package com.lunatech.slack.client.services
 
 import akka.stream.Materializer
-import play.api.libs.json.JsValue
+import com.lunatech.slack.client.api.SlackClient
+import play.api.libs.json.{JsValue, Reads}
 import play.api.libs.ws.JsonBodyWritables._
-import play.api.libs.ws.StandaloneWSResponse
 import play.api.libs.ws.ahc.StandaloneAhcWSClient
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 class SlackCaller(implicit materializer: Materializer) {
 
   val wsClient = StandaloneAhcWSClient()
 
-  def makeApiCall(token: String, url: String, body: JsValue)(implicit ec: ExecutionContext): Future[StandaloneWSResponse] = {
+  def makeApiCall[T](token: String, url: String, body: JsValue)(implicit ec: ExecutionContext, reads: Reads[T]): Future[T] = {
     wsClient.url(url)
       .withHttpHeaders(
         "Content-Type" -> "application/json",
         "Authorization" -> s"Bearer $token")
       .post(body)
+      .flatMap(x => SlackClient.jsonToClass[T](x.body) match {
+        case Success(c) => Future.successful(c)
+        case Failure(error) => Future.failed(error)
+      })
   }
 
-  def makeGetApiCall(token: String, url: String, params: Map[String, String])(implicit ec: ExecutionContext): Future[StandaloneWSResponse] = {
+  def makeGetApiCall[T](token: String, url: String, params: Map[String, String])(implicit ec: ExecutionContext, reads: Reads[T]): Future[T] = {
     wsClient.url(url)
       .withHttpHeaders(
         "Content-Type" -> "application/json",
         "Authorization" -> s"Bearer $token")
       .withQueryStringParameters(params.toSeq: _*)
       .get()
+      .flatMap(x => SlackClient.jsonToClass[T](x.body) match {
+        case Success(c) => Future.successful(c)
+        case Failure(error) => Future.failed(error)
+      })
   }
 }
